164a7701650c2c5185b047ded0d24fe43ad637c3
ValientRH <151187840+ValientRH@users.noreply.github.com>
2024-10-09 15:57:55 +0530

    Time: 2 ms (44.39%) | Memory: 10.6 MB (77.38%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for (int i =nums.size()-1;i>=0;i--)
+        {
+            if(nums[i]== val){
+                nums.erase(nums.begin()+i);
+            }
+        }
+        int k = nums.size();
+        return k;
+    }
+};
