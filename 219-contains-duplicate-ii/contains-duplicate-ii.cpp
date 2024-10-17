c0c3fd935651c94059ebe2eba39058c4e76c7557
ValientRH <151187840+ValientRH@users.noreply.github.com>
2024-10-17 17:09:10 +0530

    Time: 2428 ms (5.04%) | Memory: 63 MB (89.35%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if (k==0){
+            return false;
+        }
+        for (int i = 0;i<nums.size()-1;i++){
+            int j = i+1;
+            while(abs(i-j)<=k && j<nums.size()){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+                j++;
+            }
+        }
+        return false;
+    }
+};
