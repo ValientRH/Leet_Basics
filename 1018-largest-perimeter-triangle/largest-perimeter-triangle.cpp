bf73b7ecff8af1039bcb044f52f58696fd398730
Shubham Kumar <151187840+ValientRH@users.noreply.github.com>
2025-10-08 16:14:09 +0530

    Time: 9 ms (34.99%) | Memory: 25.7 MB (15.21%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -1,3 +1,24 @@
+int partion(vector<int>& arr,int low , int high){
+    int pivot = arr[high];
+    int i = low-1;
+    for(int j = low;j<high;j++ ){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[high]);
+    return i+1 ;
+}
+void quick(vector<int>& arr,int low , int high){
+    if(low<high){
+        int pi = partion(arr,low,high);
+
+        quick(arr,low,pi-1);
+        quick(arr,pi+1,high);
+    }
+}
+
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
@@ -9,15 +30,16 @@ public:
             return 0;
         }
         // sort(nums.begin(),nums.end());
-        for(int i = 1;i<n;i++){
-            int key = nums[i];
-            int j = i-1;
-            while(j>=0&&nums[j]>key){
-                nums[j+1]=nums[j];
-                j--;
-            }
-            nums[j+1] = key;
-        }
+        // for(int i = 1;i<n;i++){
+        //     int key = nums[i];
+        //     int j = i-1;
+        //     while(j>=0&&nums[j]>key){
+        //         nums[j+1]=nums[j];
+        //         j--;
+        //     }
+        //     nums[j+1] = key;
+        // }
+        quick(nums,0,n-1);
         for(int i = n-1;i>=2;i--){
             if(nums[i]<nums[i-1]+nums[i-2]){
                 return nums[i]+nums[i-1]+nums[i-2];
