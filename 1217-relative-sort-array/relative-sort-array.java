f5581775fb085a6eedd0f15c35a5a523e65c3a0b
Shubham Kumar <151187840+ValientRH@users.noreply.github.com>
2025-10-25 23:02:11 +0530

    Time: 2 ms (67.02%) | Memory: 42 MB (86.09%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        List<Integer> result = new ArrayList<>();
+        int a1 = arr1.length;
+        int a2 = arr2.length;
+        
+        int[] res = new int[a1];
+        int idx = 0;
 
-        // Traverse through the relative order array
         for (int i = 0; i < arr2.length; i++) {
             // Traverse through the target array
             for (int j = 0; j < arr1.length; j++) {
                 // If element in target array matches with relative order element
                 if (arr1[j] == arr2[i]) {
                     // Add it to the result array
-                    result.add(arr1[j]);
+                     res[idx++] = arr1[j];
                     // Mark the element in target array as visited
                     arr1[j] = -1;
                 }
             }
         }
-
-        // Sort the remaining elements in the target array
         Arrays.sort(arr1);
         // Add the remaining elements to the result array
         for (int i = 0; i < arr1.length; i++) {
             if (arr1[i] != -1) {
-                result.add(arr1[i]);
+               res[idx++] = arr1[i];
             }
         }
-
-        // Convert ArrayList to array
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return res;
     }
 }
